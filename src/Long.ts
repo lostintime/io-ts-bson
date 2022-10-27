3cdce9f276bba875056c0a3eb2039faf95b7c0f4
lostintime <lostintime.dev@gmail.com>
2022-10-27 20:40:58 +0200

    chore: add try/catch to Long codec

--- src/Long.ts
+++ src/Long.ts
@@ -7,12 +7,16 @@ export const Long = new t.Type<number, Long, unknown>(
   'Long',
   t.number.is,
   (i, c) => {
-    if (t.number.is(i)) {
-      return t.success(i);
-    } else if (i instanceof bson.Long) {
-      return t.success(i.toNumber());
-    } else {
-      return t.failure(i, c, `Failed to decode Long: bson.Long | number expected, got ${typeof i}`);
+    try {
+      if (t.number.is(i)) {
+        return t.success(i);
+      } else if (i instanceof bson.Long) {
+        return t.success(i.toNumber());
+      } else {
+        return t.failure(i, c, `Failed to decode Long: bson.Long | number expected, got ${typeof i}`);
+      }
+    } catch (e: unknown) {
+      return t.failure(i, c, `Failed to decode Long: ${e}`);
     }
   },
   (a) => bson.Long.fromNumber(a),
