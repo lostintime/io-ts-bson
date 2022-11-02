6819b8d7ae66dd555cf624f05ddf278fa5fa85b3
lostintime <lostintime.dev@gmail.com>
2022-11-02 12:00:02 +0100

    feat: add Decimal128 codec

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './BigIntFromLong';
 export * from './Binary';
 export * from './date';
+export * from './Decimal128';
 export * from './NumberFromInt32';
 export * from './NumberFromLong';
 export * from './ObjectId';
