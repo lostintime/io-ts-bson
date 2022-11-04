8dc9b1e14d5eca6a9021b78c5382e6ebb8bff485
lostintime <lostintime.dev@gmail.com>
2022-11-04 08:37:21 +0100

    test: some more ObjectIdString branches

--- test/ObjectIdString.test.ts
+++ test/ObjectIdString.test.ts
@@ -1,4 +1,4 @@
-import { ObjectIdString } from '../src/ObjectIdString';
+import { isObjectIdString, ObjectIdString } from '../src/ObjectIdString';
 import * as bson from 'bson';
 import { expect } from 'chai';
 import * as t from 'io-ts';
@@ -12,5 +12,17 @@ describe('ObjectIdString', () => {
   it('encodes to ObjectId', () => {
     const o = bson.ObjectId.createFromHexString('635ac5f39d51a41c657c3587');
     expect(ObjectIdString.encode('635ac5f39d51a41c657c3587' as ObjectIdString).equals(o)).is.true;
+  });
+
+  describe('isObjectIdString', () => {
+    it('returns true for valid ObjectId strings', () => {
+      expect(isObjectIdString('635ac5f39d51a41c657c3587')).is.true;
+      expect(ObjectIdString.is('635ac5f39d51a41c657c3587')).is.true;
+    });
+
+    it('returns false for invalid ObjectId strings', () => {
+      expect(isObjectIdString('abc123')).is.false;
+      expect(ObjectIdString.is('abc123')).is.false;
+    });
   });
 });
