c16204dd457ecb8b11b0fcf275a6839f563b2a6e
lostintime <lostintime.dev@gmail.com>
2022-11-04 08:42:39 +0100

    test: some more ObjectId branches

--- test/ObjectId.test.ts
+++ test/ObjectId.test.ts
@@ -13,4 +13,21 @@ describe('ObjectId', () => {
     const o = new bson.ObjectId();
     expect(ObjectId.encode(o)).equals(o);
   });
+
+  it('fails to decode from non ObjectId values', () => {
+    const n = Number(20);
+    const r = ObjectId.decode(n);
+    expect(r._tag).equals('Left');
+  });
+
+  it('returns true for ObjectId values to is function', () => {
+    const o = new bson.ObjectId();
+    expect(ObjectId.is(o)).is.true;
+  });
+
+  it('returns false for non ObjectId values to is function', () => {
+    const n = Number(10);
+    expect(ObjectId.is(n)).is.false;
+    expect(ObjectId.is(10)).is.false;
+  });
 });
